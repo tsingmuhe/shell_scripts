d20aad5ef44ce8499906510e299e3d966feab9d4
sunchangpeng <sunchangpeng@bytedance.com>
2021-09-24 23:04:57 +0800

    Update mysql_install.sh

--- mysql_install.sh
+++ mysql_install.sh
@@ -12,41 +12,37 @@ MYSQL_USER="mysql"
 if [ ! -f ${TAR_NAME} ]; then
   wget -c https://cdn.mysql.com//Downloads/MySQL-8.0/mysql-8.0.26-linux-glibc2.17-x86_64-minimal-rebuild.tar.xz -O ${TAR_NAME}
 fi
+echo "check tar file ... ok"
 
 # check user&group
-echo "--------------------> check mysql user... "
 id -u ${MYSQL_USER} &>/dev/null
 if [ $? -ne 0 ]; then
   groupadd ${MYSQL_GROUP}
   useradd -g ${MYSQL_GROUP} -s /sbin/nologin -d /home/${MYSQL_USER} ${MYSQL_USER}
 fi
-echo "-------------------->ok"
+echo "check mysql user ... ok"
 
 # check install dir
-echo "--------------------> check install dir ..."
 [ ! -d "${INSTALL_DIR}" ] && mkdir -p ${INSTALL_DIR}
 [ ! -d "${DATA_DIR}" ] && mkdir -p ${DATA_DIR}
 
 chown -R root ${INSTALL_DIR}
 chown -R mysql:mysql ${INSTALL_DIR}
 chown -R mysql:mysql ${DATA_DIR}
-echo "--------------------> ok"
+echo "check install dir ... ok"
 
 # untar file
-echo "--------------------> untar file ..."
-tar -xvf ${TAR_NAME} -C ${INSTALL_DIR} --strip-components 1
-echo "--------------------> ok"
+tar -xf ${TAR_NAME} -C ${INSTALL_DIR} --strip-components 1
+echo "untar file ... ok"
 
 # init db and config
-echo "--------------------> init db..."
 ${INSTALL_DIR}/bin/mysqld --initialize-insecure --user=${MYSQL_USER} --basedir=${INSTALL_DIR} --datadir=${DATA_DIR}
 [ $? -ne 0 ] && exit 1
 cp ${INSTALL_DIR}/support-files/mysql.server /etc/init.d/mysqld
 chmod +x /etc/init.d/mysqld
-echo "--------------------> ok"
+echo "init database ... ok"
 
 # init my.cnf
-echo "--------------------> init my.cnf..."
 [ -f /etc/my.cnf ] && mv /etc/my.cnf{,.ori}
 cat >>/etc/my.cnf <<EOF
 [client]
@@ -58,4 +54,4 @@ default-character-set=utf8
 port= 3306
 server-id = 1
 EOF
-echo "--------------------> ok"
+echo "init my.cnf ... ok"
